4862195d3a9921128bd04f699b826de06c0f43ab
Miguel Casas-Sanchez <mcasas@chromium.org>
2013-12-01 19:49:48 +0100

    Got all feeds rendering OK in something perspective-like.

--- webgl.js
+++ webgl.js
@@ -2,99 +2,119 @@
 // Adapted from http://stemkoski.github.io/Three.js/#webcam-texture
 // MAIN
 
-// standard global variables
+// ThreeJS global variables.
 var container, scene, camera, renderer;
 
-// custom global variables
-var video, videoImage, videoImageContext, videoTexture;
+// Video/canvas use global variables.
+var NUM_CAMERAS = 3;
+var video = [];
+var videoImage = [];
+var videoImageContext = [];
+var videoTexture = [];
 
+// Entry point of the webgl.js file. Call this and then
 function startWebGL() {
   init();
   animate();
 }
 
-// FUNCTIONS 		
-function init() 
+function init()
 {
-	// SCENE
-	scene = new THREE.Scene();
-	
-	// CAMERA
-	var canvas = document.getElementById("glcanvas");
-	var SCREEN_WIDTH = 960;
-    var SCREEN_HEIGHT = 240;
-	
-	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 1000;
-	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-	scene.add(camera);
-	camera.position.set(0,150,400);
-	camera.lookAt(scene.position);	
-	
-	// RENDERER
-	renderer = new THREE.CanvasRenderer(); 
-	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-	
-	container = document.getElementById("glcanvas");
-	container.appendChild( renderer.domElement );
-	// CONTROLS
-	//controls = new THREE.OrbitControls( camera, renderer.domElement );
-	// LIGHT
-	var light = new THREE.PointLight(0xffffff);
-	light.position.set(0,250,0);
-	scene.add(light);
-	
-    // FLOOR
-	var floorTexture = new THREE.ImageUtils.loadTexture( 'checkerboard.jpg' );
-	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
-	floorTexture.repeat.set( 10, 10 );
-	var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
-	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-	floor.position.y = -0.5;
-	floor.rotation.x = Math.PI / 2;
-	scene.add(floor);	
-	
-	// VIDEO //
-	video = document.getElementById('view1');
-	
-	videoImage = document.getElementById( 'canvas1' );
-	videoImageContext = videoImage.getContext( '2d' );
-	// background color if no video present
-	videoImageContext.fillStyle = '#000000';
-	videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
+  // New scene needed.
+  scene = new THREE.Scene();
 
-	videoTexture = new THREE.Texture( videoImage );
-	videoTexture.minFilter = THREE.LinearFilter;
-	videoTexture.magFilter = THREE.LinearFilter;
-	
-	var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
-	// the geometry on which the movie will be displayed;
-	// 		movie image will be scaled to fit these dimensions.
-	var movieGeometry = new THREE.PlaneGeometry( 133, 100, 1, 1 );
-	var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
-	movieScreen.position.set(0,50,0);
-	scene.add(movieScreen);
-	
-	camera.position.set(0,50,300);
-	camera.lookAt(movieScreen.position);
-				
-	console.log("Three GL context and data initialized.");	
+  // Retrieve canvas and Field Of View constants.
+  container = document.getElementById("glcanvas");
+  var SCREEN_WIDTH = 960;
+  var SCREEN_HEIGHT = 240;
+  var VIEW_ANGLE = 45;
+  var ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+  var NEAR = 0.1;
+  var FAR = 1000;
+
+  // Scene camera.
+  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+  scene.add(camera);
+  camera.position.set(0,150,400);
+  camera.lookAt(scene.position);
+  console.log("Three.JS camera initialized");
+
+  // Renderer.
+  renderer = new THREE.CanvasRenderer();
+  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  container.appendChild( renderer.domElement );
+  console.log("Three.JS renderer initialized");
+
+  // Light source.
+  var light = new THREE.PointLight(0xffffff);
+  light.position.set(0,250,0);
+  scene.add(light);
+  console.log("Three.JS light source initialized");
+
+  // FLOOR
+  //var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
+  //floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+  //floorTexture.repeat.set( 10, 10 );
+  //var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+  //var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+  //var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  //floor.position.y = -0.5;
+  //floor.rotation.x = Math.PI / 2;
+  //scene.add(floor);
+
+  // Camera video input. The idea is to plug a camera <video> feed into a canvas
+  // and use it to retrieve the data.
+  var movieScreen = [];
+  for (var i=0; i < NUM_CAMERAS; i++) {
+    video[i] = document.getElementById('view' + (i+1));
+
+    videoImage[i] = document.getElementById('canvas' + (i+1));
+    videoImageContext[i] = videoImage[i].getContext('2d');
+    // background color if no video present
+    videoImageContext[i].fillStyle = '#000000';
+    videoImageContext[i].fillRect( 0, 0, videoImage[0].width, videoImage[0].height);
+
+    videoTexture[i] = new THREE.Texture(videoImage[i]);
+    videoTexture[i].minFilter = THREE.LinearFilter;
+    videoTexture[i].magFilter = THREE.LinearFilter;
+
+    // Here is the magic: assign the video as a material of basic mesh.
+    var movieMaterial = new THREE.MeshBasicMaterial(
+        {map: videoTexture[i], overdraw: true, side:THREE.DoubleSide});
+    var movieGeometry =
+        new THREE.PlaneGeometry(videoImage[0].width, videoImage[0].height, 1, 1);
+    movieScreen[i] = new THREE.Mesh(movieGeometry, movieMaterial);
+
+    movieScreen[i].position.set(videoImage[0].width*(i-1), 50, 50*Math.abs(i-1));
+    movieScreen[i].rotation.set(0, (Math.PI /8)*(1-i), 0);
+    scene.add(movieScreen[i]);
+  }
+  camera.position.set(0,50,400);
+  if (NUM_CAMERAS >1)
+    camera.lookAt(movieScreen[1].position);
+  else
+    camera.lookAt(movieScreen[0].position);
+  console.log("Three.JS GL context and video feeds initialized.");
 }
 
 function animate() {
-    requestAnimationFrame( animate );
-	render();		
-	update();
+  requestAnimationFrame(animate);
+  render();
+  update();
 }
 
-function update() {		
+function render()  {
+  for (var i=0; i < NUM_CAMERAS; i++) {
+    if (video[i].readyState === video[i].HAVE_ENOUGH_DATA) {
+       videoImageContext[i].drawImage(
+       	  video[i], 0, 0, videoImage[i].width, videoImage[i].height);
+    if (videoTexture[i])
+      videoTexture[i].needsUpdate = true;
+    }
+  }
+  renderer.render(scene, camera);
 }
 
-function render()  {	
-	if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
-		videoImageContext.drawImage( video, 0, 0, videoImage.width, videoImage.height );
-		if ( videoTexture ) 
-			videoTexture.needsUpdate = true;
-	}
-	renderer.render( scene, camera );
+function update() {
+  // Empty for the moment.
 }
